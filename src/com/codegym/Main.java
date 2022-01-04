bb52827ab7e1d5261769da7e83eab43cf13b626c
CGAnVil <hoangminh.tlu@gmail.com>
2022-01-04 16:59:01 +0700

    upload bai tap

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,30 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        System.out.println("Enter a:");
+        a = sc.nextInt();
+        System.out.println("Enter b:");
+        b = sc.nextInt();
+        a = Math.abs(a);
+        b = Math.abs(b);
+        if (a == 0 || b == 0) {
+            System.out.println("No greatest common factor");
+        } else {
+            while (a != b) {
+                if (a > b) {
+                    a = a - b;
+                } else {
+                    b = b - a;
+                }
+            }
+            System.out.println("Greatest common factor:" + a);
+        }
+
+    }
+}
